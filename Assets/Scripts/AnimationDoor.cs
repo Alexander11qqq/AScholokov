83bf59b17f6bc66a1bf9bf415d62b8ad9574b0a5
Alexander11qqq <aleksandrov27@mail.ru>
2020-03-17 18:02:32 +0300

    latin

---
+++ Assets/Scripts/AnimationDoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class AnimationDoor : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var animator = GetComponent<Animator>();
+        animator.SetTrigger("Privet");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
